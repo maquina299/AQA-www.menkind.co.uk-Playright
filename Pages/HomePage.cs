df5f3f77f3b8c8cc5bdc845e7766c5c5c66e0c3c
maquina299 <70115151+maquina299@users.noreply.github.com>
2025-01-15 19:49:03 +0300

    Add project files.

---
+++ Pages/HomePage.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using www.menkind.co.uk.Pages;
+using NLog;
+
+namespace www.menkind.co.uk.Tests
+{
+    [TestFixture]
+    public class HomePageTests
+    {
+        private IWebDriver _driver;
+        private HomePageObject _homePage;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+            
+            Console.WriteLine("Initializing ChromeDriver...");
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--remote-debugging-port=9222");
+            _driver = new ChromeDriver();
+            Console.WriteLine("ChromeDriver initialized.");
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            _driver.Manage().Window.Maximize();
+            _homePage = new HomePageObject(_driver);
+            _driver.Navigate().GoToUrl("https://www.menkind.co.uk");
+            // Закрываем модальные окна
+            _homePage.HandleModals();
+             // Ожидаем появления кнопки "Allow all Cookies" по классу
+            //WebDriverWait wait = new (_driver, TimeSpan.FromSeconds(10));
+            //IWebElement cookieButton = wait.Until(driver => driver.FindElement(By.ClassName("css-a0j149")));
+
+            // Нажимаем на кнопку
+           // cookieButton.Click();
+        }
+
+        [Test]
+        public void Test_HomePageLoadsSuccessfully()
+        {
+            Logger.Info("Executing Test_HomePageLoadsSuccessfully");
+            Assert.Multiple(() =>
+            {
+                Assert.That(_homePage.IsLogoDisplayed(), Is.True, "Logo is not displayed");
+                Assert.That(_homePage.GetTitle(), Is.EqualTo("Menkind | Unique Gadgets & Gifts for Men"), "Page title does not contain 'Menkind'");
+            });
+            Logger.Info("Test completed successfully");
+        }
+
+
+     [TearDown]
+     public void TearDown() {_driver?.Quit(); _driver?.Dispose(); }
+     }
+}
---
+++ Pages/HomePageObjects.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+
+namespace www.menkind.co.uk.Pages
+{
+    public class HomePageObject
+    {
+        private readonly IWebDriver _driver;
+
+        public HomePageObject(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void HandleModals()
+        {
+            try
+            {
+                // Закрытие окна Discount
+                var discountModalCloseButton = _driver.FindElement(By.XPath("//button[contains(text(), 'No Thanks. Close Form')]"));
+                if (discountModalCloseButton.Displayed)
+                {
+                    discountModalCloseButton.Click();
+                    Console.WriteLine("Discount modal closed.");
+                }
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Discount modal not found.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while handling discount modal: {ex.Message}");
+            }
+            try
+            {
+                // Закрытие окна Cookies
+                var cookiesModal = _driver.FindElement(By.XPath("//button[contains(text(), 'Allow all Cookies')]"));
+                if (cookiesModal.Displayed)
+                {
+                    var acceptCookiesButton = cookiesModal.FindElement(By.XPath("//button[contains(text(), 'Allow all Cookies')]"));
+                    acceptCookiesButton.Click();
+                    Console.WriteLine("Cookies modal closed.");
+                }
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Cookies modal not found.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while handling cookies modal: {ex.Message}");
+            }
+
+            
+        }
+
+        public bool IsLogoDisplayed()
+        {
+            return _driver.FindElement(By.CssSelector("a.header__logo")).Displayed;
+        }
+
+        public string GetTitle()
+        {
+            return _driver.Title;
+        }
+    }
+}
