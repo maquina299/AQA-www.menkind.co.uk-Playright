30e2871e3bc3dbccd0dd0e373d389797807ad3c9
maquina299 <70115151+maquina299@users.noreply.github.com>
2025-01-20 15:29:24 +0300

    registration tests added, some refactoring

--- Tests/HomePage.cs
+++ Tests/HomePage.cs
@@ -3,41 +3,39 @@ using www.menkind.co.uk.Pages;
 using www.menkind.co.uk.Base;
 
 namespace www.menkind.co.uk.Tests
-
 {
-    public class HomePage : BasePage
-    {
-        public HomePage(IWebDriver driver) : base(driver)
-        {
-        }
-
-        // Другие методы, специфичные для HomePage
-    }
     [TestFixture]
     public class HomePageTests
     {
+        // Changed: Removed HomePage _homePage as it's no longer needed
+        // Added: _basePage for handling common operations
         private IWebDriver _driver;
-        private HomePage _homePage;
+        private BasePage _basePage;
+        private HomePageObject _homePageObject;
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-
         [SetUp]
-        
-            public void Setup()
+        public void Setup()
         {
-
-
             Console.WriteLine("Initializing ChromeDriver...");
-            ChromeOptions options = new ChromeOptions();
+            ChromeOptions options = new();
             options.AddArgument("--remote-debugging-port=9222");
-            _driver = new ChromeDriver();
+            _driver = new ChromeDriver(options);
             Console.WriteLine("ChromeDriver initialized.");
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             _driver.Manage().Window.Maximize();
-            _homePage = new HomePage(_driver);
-            _driver.Navigate().GoToUrl("https://www.menkind.co.uk");
-            _homePage.HandleModals();
 
+            // Changed: Initialized BasePage instead of HomePage
+            // This allows us to use common functionality like HandleModals
+            _basePage = new BasePage(_driver);
+
+            // Added: Initialized HomePageObject
+            // This allows us to use home page specific methods
+            _homePageObject = new HomePageObject(_driver);
+
+            _driver.Navigate().GoToUrl("https://www.menkind.co.uk");
+            // Changed: Use _basePage to handle modals instead of _homePage
+            _basePage.HandleModals();
         }
 
         [Test]
@@ -46,14 +44,20 @@ namespace www.menkind.co.uk.Tests
             Logger.Info("Executing Test_HomePageLoadsSuccessfully");
             Assert.Multiple(() =>
             {
-                Assert.That(_homePageObjects.IsLogoDisplayed(), Is.True, "Logo is not displayed");
-                Assert.That(_homePage.GetTitle(), Is.EqualTo("Menkind | Unique Gadgets & Gifts for Men"), "Page title does not contain 'Menkind'");
+                // Changed: Use _homePageObject instead of _homePage
+                // This allows us to use specific methods defined in HomePageObject
+                Assert.That(_homePageObject.IsLogoDisplayed(), Is.True, "Logo is not displayed");
+                //Assert.That(_homePageObject.IsLogoLoaded(), Is.True, "Logo is not properly loaded or may be corrupted");
+                Assert.That(_homePageObject.GetTitle(), Is.EqualTo("Menkind | Unique Gadgets & Gifts for Men"), "Page title does not match expected");
             });
             Logger.Info("Test completed successfully");
         }
 
-
         [TearDown]
-        public void TearDown() { _driver?.Quit(); _driver?.Dispose(); }
+        public void TearDown()
+        {
+            _driver?.Quit();
+            _driver?.Dispose();
+        }
     }
 }
